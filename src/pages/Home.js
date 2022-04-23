c38b6f2e0e127770032bef0969745353f0f2169b
Ajith K.R <46483037+ajithkr12@users.noreply.github.com>
2022-04-23 13:02:15 +0530

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from "../components/Header";
 import Service from "../components/Service";
-import bgvideo from "../images/home.mp4";
 import Footer from '../components/Footer';
 
 
